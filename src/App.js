f5a56dcaf160667e07393bca222f0a0a851610c9
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-10-18 19:07:19 +0200

    fetch done

--- src/App.js
+++ src/App.js
@@ -5,32 +5,37 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      allData: []
+      allPokeData: []
     }
   }
 
-  componentDidMount() {
-    this.getAllData();
-  }
-
-  getAllData = () => {
-    fetchPokemons()
-      .then(data => {
-        return (
-          this.setState({
-            allData: data.results,
-          })
-        );
-      })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        hola
-      </div>
-    );
-  }
+componentDidMount() {
+  this.getAllData();
 }
+
+getAllData = () => {
+  fetchPokemons()
+    .then(data => {
+      const allPokeInfo = data.results.map((item) => {
+        return fetch(item.url)
+          .then(response => response.json());
+      });
+      return Promise.all(allPokeInfo);
+    })
+    .then(info => console.log(info))
+
+}
+
+render() {
+  return (
+    <div className="app">
+      <ul className="app__list">
+        hola
+        </ul>
+    </div>
+  );
+}
+}
+
 
 export default App;
--- src/services/fetchPokemons.js
+++ src/services/fetchPokemons.js
@@ -1,4 +1,4 @@
-const ENDPOINT = 'https://pokeapi.co/api/v2/ability/?limit=25&offset=25';
+const ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/?limit=25';
 
 const fetchPokemons = () => {return (fetch(ENDPOINT).then(response => response.json()));};
 
