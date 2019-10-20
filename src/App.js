dccb5c87735e0df8ed31429f9b0f516d5bc2cdec
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-10-20 10:51:07 +0200

    filter done

--- src/App.js
+++ src/App.js
@@ -5,36 +5,77 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      allPokeData: []
+      allPokeData: [],
+      loaded: false
     }
   }
 
-componentDidMount() {
-  this.getAllData();
-}
+  componentDidMount() {
+    this.getAllData();
+  }
 
-getAllData = () => {
-  fetchPokemons()
-    .then(data => {
-      const allPokeInfo = data.results.map((item) => {
-        return fetch(item.url)
-          .then(response => response.json());
-      });
-      return Promise.all(allPokeInfo);
+  getAllData = () => {
+    fetchPokemons()
+      .then(data => {
+        const allPokeInfo = data.results.map((item) => {
+          return fetch(item.url)
+            .then(response => response.json());
+        });
+        return Promise.all(allPokeInfo);
+      })
+      .then(info => {
+        return (
+          this.setState({
+            allPokeData: info,
+            loaded: true,
+            nameToFilter: ''
+          })
+        );
+      })
+  }
+
+  getFilterValue = (event) => {
+    const currentName = event.currentTarget.value;
+    console.log(currentName);
+    this.setState({
+      nameToFilter: currentName
     })
-    .then(info => console.log(info))
+  }
 
-}
-
-render() {
-  return (
-    <div className="app">
-      <ul className="app__list">
-        hola
+  render() {
+    const { allPokeData, nameToFilter } = this.state;
+    return (
+      <div className="app">
+        <input type="text" className="app__filter--field" onChange={this.getFilterValue}/>
+        <ul className="app__list">
+          {allPokeData
+          .filter( poke => poke.name.toUpperCase().includes(nameToFilter.toUpperCase()))
+          .map((poke) => {
+            return (
+              <li className="list__item" key={poke.id}>
+                <div className="item--container">
+                  <img className="item--img" alt={`img--${poke.name}`} src={poke.sprites.front_default} />
+                  <p className="item--id">{`ID/${poke.id}`}</p>
+                  <h2 className="item--name">{poke.name}</h2>
+                  <ul className="item--type__list">
+                    {poke.types.map((type, index) => {
+                      return (
+                        <li className="type--item" key={`${type}--${index}`}>
+                          <div className="type--container">
+                            <p className="type--name">{type.type.name}</p>
+                          </div>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
         </ul>
-    </div>
-  );
-}
+      </div>
+    );
+  }
 }
 
 
